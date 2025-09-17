7e420329888fc22b5796c8e3e23f108a503a2db2
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-09-17 13:46:54 -0400

    Added fully fleshed implementation using forms instead

--- Django/website/blog/admin.py
+++ Django/website/blog/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from blog.models import User, Post
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Post)
--- Django/website/website_live/urls.py
+++ Django/website/website_live/urls.py
@@ -1,7 +1,8 @@
-"""website URL Configuration
+"""
+URL configuration for website_live project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),    
+    path("admin/", admin.site.urls),
+    path("blog/", include("blog.urls")),  # Include URLs from the blog app
 ]
-
