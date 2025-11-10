63ac5d7da8b64f68cfa36315354db4c16721f7f9
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-11-10 13:59:33 -0500

    Code to be demo-ed added

---
+++ DynamicAnalyzer/z3_example_live.py
@@ -0,0 +1,15 @@
+import z3
+
+# Create symbols
+
+
+# instantiates the solver
+
+# Creates the constraints
+# (x0 = 2y0 ⋀ x0 > y0 + 10)
+
+
+# attempt to solve the constraint and print the solution if satisfiable
+
+
+
