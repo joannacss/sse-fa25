eec3564a8a3476d78b075f0c5043d38e89ecee4b
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-11-10 10:44:54 -0500

    Added random fuzzer from prior lecture

--- DynamicAnalyzer/random_fuzzer.py
+++ DynamicAnalyzer/random_fuzzer.py
@@ -1,65 +1,50 @@
 from examples.triangle import triangle
 import sys
 import inspect
-from random import randrange
+import random
+
+seen_lines = set()
+def get_code(function_code, lineno) -> str:
+    # 0...N
+    source_lines, start_index = inspect.getsourcelines(function_code)
+    return source_lines[lineno - start_index].strip()
 
 
 def analyze(frame, event, arg):
-	# get function code
-	function_code = frame.f_code
+    # function name?
+    function_code = frame.f_code
+    function_name = function_code.co_name
+    # line number?
+    lineno = frame.f_lineno
+    code_line = get_code(function_code, lineno)
+    local_variables = frame.f_locals
+    variable_values = []
+    for v in local_variables:
+        variable_values.append(f"{v} = {local_variables[v]}")
+    seen_lines.add(lineno)
+    print(f"{function_name}:{lineno} ({event})")
+    print(f"\t{code_line}")
+    print(f"\t\t{variable_values}")
 
-	# get function name
-	function_name = function_code.co_name
+    return analyze
 
-	# get line number
-	lineno = frame.f_lineno
-	
-	# TODO: get all source lines
-	source_lines, start  = inspect.getsourcelines(function_code)
-	statement = source_lines[lineno - start].strip()
-	
-	# TODO: get all variables and print their values
-	variables = []
-	for v in frame.f_locals:
-		variables.append(f"{v}={frame.f_locals[v]}")
-	variables = ",".join(variables)
-
-
-	# TODO: prints them f"{event} --> {function_name}:{lineno} {statement} ({variables})"
-	print(f"{event} --> {function_name}:{lineno} {statement} ({variables})")
-
-	visited_lines.add(lineno)
-	# returns the function itself to track the new scope
-	return analyze
-    
-    
 
 def main():
-	
-	num_attempts = 0
-	threshold = 200
-	while True:
-		a = randrange(1, 5)
-		b = randrange(1, 5)
-		c = randrange(1, 5)
-		# start tracing the invocation
-		print("START")
-		sys.settrace(analyze)
-		output = triangle(a, b, c)
-		# stop tracing the invocation
-		sys.settrace(None)
-		print("END")
-		if len(visited_lines) == total_lines-4:
-			print("100% coverage!")
-			break
-		num_attempts += 1
-		if num_attempts > threshold:
-			break
 
-	print("Output at main()", output)
-    
+    # generate inputs: hint: use the random module
+    # generate input iteratively, until all of triangle's code lines are executed
+    # E-mail subject: Lecture 16 - Extra Credit (2PM)
+    triangle(2, 2, 1)
+    while len(seen_lines) < 10:
+        a = random.randint(1, 11)
+        b = random.randint(1, 11)
+        c = random.randint(1, 11)
+        sys.settrace(analyze)
+        triangle(a, b, c)
+        sys.settrace(None)
+    print(seen_lines)
+    print("DONE")
+
 
 if __name__ == '__main__':
-	total_lines = len(inspect.getsourcelines(triangle)[0])
-	visited_lines = set()
-	main()
+    main()
